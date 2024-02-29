77db36f0e5f0aae45c1b10acb8bf756861ca90ef
Jeff_Huang <u20010630@gmail.com>
2024-03-01 01:52:35 +0800

    [ADD] add background images and change setting of signup page

--- app/src/main/java/com/arctic/aurora/SignUpActivity.java
+++ app/src/main/java/com/arctic/aurora/SignUpActivity.java
@@ -3,6 +3,8 @@ package com.arctic.aurora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Button signup = findViewById(R.id.btn_signup);
+        signup.setOnClickListener(btn_signup);
     }
+
+    private final View.OnClickListener btn_signup = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            System.out.println(v);
+        }
+    };
 }
