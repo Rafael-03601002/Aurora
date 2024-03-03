b9080e1e019a0cb9359668111e69a87523f8121b
Jeff_Huang <u20010630@gmail.com>
2024-03-03 16:20:19 +0800

    [ADD] home page activity firebase authentiction initialize

--- app/src/main/java/com/arctic/aurora/HomePageActivity.java
+++ app/src/main/java/com/arctic/aurora/HomePageActivity.java
@@ -2,13 +2,27 @@ package com.arctic.aurora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomePageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // System auto-generated
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        // Firebase user authentication
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser == null) {
+            startActivity(new Intent(HomePageActivity.this, LoginActivity.class));
+            finish();
+        }
     }
 }
