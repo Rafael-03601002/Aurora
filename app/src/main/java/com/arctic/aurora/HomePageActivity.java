0177e460ee68ad3c0a75e7771cca8373dbb5b93a
Jeff_Huang <u20010630@gmail.com>
2024-03-11 23:44:58 +0800

    [ADJ] adjust dynamic generate program

--- app/src/main/java/com/arctic/aurora/HomePageActivity.java
+++ app/src/main/java/com/arctic/aurora/HomePageActivity.java
@@ -1,24 +1,27 @@
 package com.arctic.aurora;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.TimerTask;
 
 public class HomePageActivity extends AppCompatActivity {
     // home page (base) objects
@@ -29,6 +32,9 @@ public class HomePageActivity extends AppCompatActivity {
 
     // custom_program objects
     private View layout_program;
+    private View layout_mart;
+    private View layout_explore;
+    private LinearLayout program_view;
     public Button monday;
     public Button tuesday;
     public Button wednesday;
@@ -36,8 +42,14 @@ public class HomePageActivity extends AppCompatActivity {
     public Button friday;
     public Button saturday;
     public Button sunday;
-    private Button add_program;
 
+    // confirm_program_dialog objects
+    private TextInputEditText sports;
+    private TextInputEditText time;
+    private AlertDialog programDialog;
+
+    // program element
+    CountDownTimer countDownTimer;
 
     @Override
     public void onStart() {
@@ -96,6 +108,8 @@ public class HomePageActivity extends AppCompatActivity {
 
         // custom_program elements
         layout_program = getLayoutInflater().inflate(R.layout.custom_program, null);
+        program_view = layout_program.findViewById(R.id.program_list);
+
         ImageView setting = setImageViewEvent(layout_program, R.id.setting, btn_setting);
         monday = setBtnEvent(layout_program, R.id.btn_monday, btn_monday);
         tuesday = setBtnEvent(layout_program, R.id.btn_tuesday, btn_tuesday);
@@ -104,7 +118,13 @@ public class HomePageActivity extends AppCompatActivity {
         friday = setBtnEvent(layout_program, R.id.btn_friday, btn_friday);
         saturday = setBtnEvent(layout_program, R.id.btn_saturday, btn_saturday);
         sunday = setBtnEvent(layout_program, R.id.btn_sunday, btn_sunday);
-        add_program = setBtnEvent(layout_program, R.id.add_program, btn_add_program);
+        Button add_program = setBtnEvent(layout_program, R.id.add_program, btn_add_program);
+
+        // custom_mart elements
+        layout_mart = getLayoutInflater().inflate(R.layout.custom_mart, null);
+
+        // custom_explore elements
+        layout_explore = getLayoutInflater().inflate(R.layout.custom_explore, null);
     }
 
     // base events
@@ -112,18 +132,21 @@ public class HomePageActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             groupSetBtnColor(mart, new Button[]{program, explore});
+            changeView(layout_mart);
         }
     };
     public View.OnClickListener btn_program = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             groupSetBtnColor(program, new Button[]{mart, explore});
+            changeView(layout_program);
         }
     };
     public View.OnClickListener btn_explore = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             groupSetBtnColor(explore, new Button[]{mart, program});
+            changeView(layout_explore);
         }
     };
 
@@ -179,7 +202,56 @@ public class HomePageActivity extends AppCompatActivity {
     public View.OnClickListener btn_add_program = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            // TODO: dynamic add program
+            // custom alert dialog
+            AlertDialog.Builder programDialogBuilder = new AlertDialog.Builder(HomePageActivity.this);
+            View custom_confirm_program = getLayoutInflater().inflate(R.layout.custom_confirm_program_dialog, null);
+            programDialogBuilder.setView(custom_confirm_program);
+            programDialog = programDialogBuilder.create();
+            programDialog.show();
+
+            // alert dialog elements and events
+            sports = custom_confirm_program.findViewById(R.id.sports);
+            time = custom_confirm_program.findViewById(R.id.text_timer);
+            Button confirm = setBtnEvent(custom_confirm_program, R.id.btn_dialog_confirm, btn_dialog_confirm);
+            Button cancel = setBtnEvent(custom_confirm_program, R.id.btn_dialog_cancel, btn_dialog_cancel);
+        }
+    };
+    public View.OnClickListener btn_dialog_confirm = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String sports_name = String.valueOf(sports.getText());
+            String timer = String.valueOf(time.getText());
+
+            View layout_program_dtl = getLayoutInflater().inflate(R.layout.program, null);
+            TextView sport_name = layout_program_dtl.findViewById(R.id.program_label);
+            TextView text_timer = layout_program_dtl.findViewById(R.id.timer);
+            ImageView program_playOrStop = layout_program_dtl.findViewById(R.id.btn_timer_control);
+            ImageView program_delete = layout_program_dtl.findViewById(R.id.btn_delete);
+
+            sport_name.setText(sports_name);
+            text_timer.setText(timer);
+            program_view.addView(layout_program_dtl);
+            programDialog.cancel();
+
+            // add onclick listener
+            program_playOrStop.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+            program_delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    program_view.removeView(layout_program_dtl);
+                }
+            });
+        }
+    };
+    public View.OnClickListener btn_dialog_cancel = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            programDialog.cancel();
         }
     };
 
@@ -208,13 +280,13 @@ public class HomePageActivity extends AppCompatActivity {
         }
         return imageView;
     }
-    public void groupSetBtnColor(Button white_btn, Button[] gray_btn) {
+    public void groupSetBtnColor(Button white_btn, @NonNull Button[] gray_btn) {
         setBtnColor(white_btn, R.color.white);
         for (Button btn : gray_btn) {
             setBtnColor(btn, R.color.gray_light);
         }
     }
-    public void setBtnColor(Button btn, int colorId) {
+    public void setBtnColor(@NonNull Button btn, int colorId) {
         btn.setTextColor(getColor(colorId));
         ((MaterialButton) btn).setIconTint(getColorStateList(colorId));
     }
